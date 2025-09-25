beaeea4c2e5148df018689c13df0c24adefc0bb5
adriantangg <adrian.tang66@gmail.com>
2025-09-25 21:02:49 +1000

    Update CommunityExperience.js

--- src/pages/CommunityExperience.js
+++ src/pages/CommunityExperience.js
@@ -9,23 +9,9 @@ function CommunityExperience({ theme }) {  // Receive the 'theme' as a prop
     {
       title: "Senior Developer",
       company: "BizTech",
-      duration: "May 2025 - Present",
-      description: "Leading development projects for the biggest technology club on campus. Working on cool things like BizTech Cards, an NFC-enabled membership card that lets users tap to check in at events or share a personalized profile",
+      duration: "Feb 2025 - Present",
+      description: "Involved with the PC time-tracker project and a Discord bot for",
       logo: biztech,
-    },
-    {
-      title: "Student Developer",
-      company: "UBC CSSS",
-      duration: "Sep. 2023 - Apr. 2024",
-      description: "Student led project for the volleyball club, where we engineered a full-stack application for 50 active club members using React, Next.js, TypeScript, and AWS to support game stat tracking and club membership registration/purchases.",
-      logo: csss,
-    },
-    {
-      title: "Software Developer",
-      company: "UBC Esports Association",
-      duration: "Sep. 2023 - Apr. 2024",
-      description: "Part of the UBCEA development team where we maintained our club website and worked on creative projects. I was involved with the PC time-tracker project and a Discord bot for our arcade machine.",
-      logo: ubcea,
     },
   ];
 
