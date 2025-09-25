272a043a2e80a3348a6eb7d26079683b25d4f99c
adriantangg <adrian.tang66@gmail.com>
2025-09-25 15:15:05 +1000

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -19,7 +19,7 @@ import nerve2 from '../images/nerve2.png';
 
 
 function Projects() {
-  const [selectedTab, setSelectedTab] = useState('Nerve');
+  const [selectedTab, setSelectedTab] = useState('ProVestia');
   const [theme, setTheme] = React.useState('light');
 
   const toggleTheme = () => {
@@ -35,19 +35,19 @@ function Projects() {
         Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
       </button>
     switch (selectedTab) {
-      case 'Nerve':
+      case 'ProVestia':
       return (
         <div className="text-image-container">
           <div className="text-content">
             <h2>Nerve</h2>
-            <p className="brief-description">Real-Time Dashboard for First Responders (Hackathon Winner)</p>
+            <p className="brief-description">Property Tech Startup</p>
             <br></br>
             <p>
-              I teamed up with three other Kevins and together we built <strong>Nerve</strong>, a real-time command dashboard for monitoring first responders in the field. Nerve streams live video and audio data from mobile clients to HQ, performs sentiment analysis on transcripts, and summarizes ongoing events in real-time. It gives HQ a full 360° situational view when every decision matters.
+              I teamed up with 4 unimelbs students and together we built <strong>ProVestia</strong>, 
             </p>
             <br></br>
             <p>
-              This project won the <strong>Startup Award</strong> and placed <strong>2nd overall</strong> at UBC BizTech’s ProductX Hackathon. I got to dive deep into full-stack dev and work with live streaming, data ingestion, AI, and real-time sockets, all in under 24 hours.
+              This project placed <strong>top 10</strong> in the Startmate Student Fellowship. I got to dive deep into full-stack dev and work with data ingestion, AI, and real-time sockets, all within 2 weeks.
             </p>
             <br></br>
             <p>
@@ -69,8 +69,8 @@ function Projects() {
             </ul>
           </div>
           <div className="image-stack">
-            <img src={nerve1} alt="nerve dashboard" />
-            <img src={nerve2} alt="nerve demo gif or image" />
+            <img src={nerve1} alt="provestia dashboard" />
+            <img src={nerve2} alt="provestia demo gif or image" />
           </div>
         </div>
       );
@@ -81,7 +81,7 @@ function Projects() {
                     <h2>This Website</h2>
                     <p className="brief-description">Personal Portfolio Project + Blog</p>
                     <br></br>
-                    <p>As a long time admirer of Naval - I've been influenced by his push for public creation. This inspired me to build a personal portoflio site to document my journey as a developer. I used this as an opportunity to learn about deploying a site via Gatsby and how it handles static site generation, customizing React components, and making everything mobile responsive.</p>
+                    <p>As a long time admirer of Tobin Tang - I've been influenced by his push for public creation. This inspired me to build a personal portoflio site to document my journey as a developer. I used this as an opportunity to learn about deploying a site via Gatsby and how it handles static site generation, customising React components, and making everything mobile responsive.</p>
                     <br></br>
                     <ul className="tech-stack">
                         <li>Gatsby</li>
@@ -97,14 +97,14 @@ function Projects() {
                 </div>
             </div>
         );
-        case 'Project Panini':
+        case 'Lotus (eToro clone)':
           return (
               <div className="text-image-container">
                   <div className="text-content">
-                      <h2>Project Panini</h2>
-                      <p className="brief-description">nwHacks 2023 Hackathon</p>
+                      <h2>Lotus</h2>
+                      <p className="brief-description">Academic Project (Relational Databases)</p>
                       <br></br>
-                      <p>While brainstorming ideas for a hackathon... we thought why not make an idea generator in itself! From there, we built a full-stack application with React that implements GPT-3's NLP model to generate project ideas based on personal interests for fellow programmers like me. We utilized user inputs to parse JSON responses from the OpenAI API calls.</p>
+                      <p>I built a social media investing app (concenptually like eToro) with SQL, PHP and Oracle for the purpose of learning databases. I gained exporsure to different ER models, database system designs, and different types of data normalisation during this process.</p>
                       <br></br>
                       <ul className="tech-stack">
                           <li>React</li>
@@ -117,95 +117,8 @@ function Projects() {
                     <img src={panini1} alt="website pic" />
                     <img src={panini2} alt="about me pic" />
                   </div>
-              </div>
-          );
-        case 'Panda Path':
-          return (
-            <div className="text-image-container">
-                <div className="text-content">
-                    <h2>Panda Path</h2>
-                    <p className="brief-description">SFU Stormhacks 2023 Hackathon</p>
-                    <br></br>
-                    <p>Panda Path was built on React Native as a mobile app to generate safe walking routes based on government crime data. JavaScript was used along with the Google Maps Platform API for app functionality, like route generation and time calculations. We built a secure back-end REST API in Express.js, connecting the application to a MongoDB database for user profiles protected by bcrypt encryption and JWT for password security.</p>
-                    <br></br>
-                    <ul className="tech-stack">
-                        <li>React Native</li>
-                        <li>MongoDB</li>
-                        <li>Express</li>
-                        <li>Node</li>
-                        <li>JWT</li>
-                    </ul>
-                </div>
-                <div className="image-stack panda-path-images">
-                    <img src={pandapic} alt="panda path" />
-                    <img src={panda2} alt="panda path" />
-                  </div>
-            </div>
+              </div
         );
-        case 'LinkedIn Clone':
-          return (
-              <div className="text-image-container">
-                  <div className="text-content">
-                      <h2>LinkedIn Clone</h2>
-                      <p className="brief-description">Academic Term Project for CPSC 304 (Relational Databases)</p>
-                      <br></br>
-                      <p>I built a social media networking app (conceptually like LinkedIn) with SQL, PHP, and Oracle for the purpose of learning databases. I gained exposure to different ER models, database system designs, and different types of data normalization during this process.</p>
-                      <br></br>
-                      <ul className="tech-stack">
-                          <li>SQL</li>
-                          <li>PHP</li>
-                          <li>Oracle</li>
-                      </ul>
-                  </div>
-                  <div className="image-stack">
-                    <img src={db1} alt="db1" />
-                    <img src={db2} alt="db2" />
-                  </div>
-              </div>
-          );
-      case 'Magic Ace':
-          return (
-              <div className="text-image-container">
-                  <div className="text-content">
-                      <h2>Magic Ace</h2>
-                      <p className="brief-description">Academic Term Project for CPSC 210 (OOP in Java)</p>
-                      <br></br>
-                      <p> I used Java to program a virtual poker chip counter for players who may have cards but are lacking chips. This idea was inspired when I travelled to The Philippines with my friends, and we had a long layover with nothing but playing cards. We wanted to play poker but did not have any chips to simulate the betting system (the fun part of poker, obviously).</p>
-                      <br></br>
-                      <ul className="tech-stack">
-                          <li>Java</li>
-                          <li>JUnit 5</li>
-                          <li>Java Swing</li>
-                          <li>JSON</li>
-                      </ul>
-                  </div>
-                  <div className="image-stack panda-path-images">
-                    <img src={poker1} alt="poker" />
-                    <img src={poker} alt="poker" />
-                  </div>
-              </div>
-          );
-      case 'QuackTrack':
-          return (
-              <div className="text-image-container">
-                  <div className="text-content">
-                      <h2>QuackTrack</h2>
-                      <p className="brief-description">Finalist in UBC BizTech Hello Hacks 2022</p>
-                      <br></br>
-                      <p>Developed a Google Chrome extension that tracked website/tab screen time. The frontend was created using Figma, HTML, CSS. The backend integrated Google APIs through JavaScript and the prototype was deployed on a Python script. I competed with Nathan Lee, Steven Huang, and Felix Ma against 15 other projects at UBC BizTech HelloHacks 2022!</p>
-                      <br></br>
-                      <ul className="tech-stack">
-                          <li>Python</li>
-                          <li>JavaScript</li>
-                          <li>HTML/CSS</li>
-                          <li>Google API</li>
-                      </ul>
-                  </div>
-                  <div className="image-stack panda-path-images">
-                    <img src={quack1} alt="quack" />
-                  </div>
-              </div>
-          );
 
          
         default:
@@ -217,26 +130,14 @@ function Projects() {
   return (
     <div className="tabbed-interface">
       <div className="tabs">
-        <div className={selectedTab === 'Nerve' ? 'active-tab' : ''} onClick={() => setSelectedTab('Nerve')}>
-          Nerve
-        </div>
-        <div className={selectedTab === 'Panda Path' ? 'active-tab' : ''} onClick={() => setSelectedTab('Panda Path')}>
-          Panda Path
+        <div className={selectedTab === 'ProVestia' ? 'active-tab' : ''} onClick={() => setSelectedTab('ProVestia')}>
+          ProVestia
         </div>
         <div className={selectedTab === 'This Website' ? 'active-tab' : ''} onClick={() => setSelectedTab('This Website')}>
           This Website
         </div>
-        <div className={selectedTab === 'Project Panini' ? 'active-tab' : ''} onClick={() => setSelectedTab('Project Panini')}>
-          Project Panini
-        </div>
-        <div className={selectedTab === 'LinkedIn Clone' ? 'active-tab' : ''} onClick={() => setSelectedTab('LinkedIn Clone')}>
+        <div className={selectedTab === 'Lotus (eToro Clone)' ? 'active-tab' : ''} onClick={() => setSelectedTab('Lotus (eToro Clone)')}>
           LinkedIn Clone
-        </div>
-        <div className={selectedTab === 'Magic Ace' ? 'active-tab' : ''} onClick={() => setSelectedTab('Magic Ace')}>
-        Magic Ace
-        </div>
-        <div className={selectedTab === 'QuackTrack' ? 'active-tab' : ''} onClick={() => setSelectedTab('QuackTrack')}>
-        QuackTrack
         </div>
       </div>
       
