9afce0b72d82dcd4141aa093ea189c748ab550ac
adriantangg <adrian.tang66@gmail.com>
2025-09-25 23:59:53 +1000

    Update gatsby-node.js

--- portfolio-main/portfolio-main/gatsby-node.js
+++ portfolio-main/portfolio-main/gatsby-node.js
@@ -3,5 +3,9 @@ const path = require('path')
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-
+pathPrefix: "/portfolio"
+  gatsby build --prefix-paths
+  "predeploy": "gatsby build --prefix-paths"
+  "deploy": "gh-pages -d public"
+  npm run deploy
 }
